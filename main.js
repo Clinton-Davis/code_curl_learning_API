8028d04f1162f08b4aae0b301082fbe115f023cb
Clinton Davis <you@example.com>
2020-01-15 14:48:43 +0000

    initail commit

---
+++ main.js
@@ -0,0 +1,10 @@
+// this code listeners
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+	if (this.readyState == 4 && this.status == 200) {
+		document.getElementById("data").innerHTML = this.responseText;
+	}
+};
+
+xhr.open("GET", "https://swapi.co/api/");
+xhr.send;
